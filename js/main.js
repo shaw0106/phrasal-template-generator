627909891ef74baf73446aedfad4c0659603c7e9
Shannon <shaw0106@algonquinlive.com>
2016-02-26 18:07:26 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var newName = alert('Let’s make a story together!');
-var newColour = prompt('Pick a colour (orange, indigo, etc.)');
-var newCreature = prompt('Pick a creature (dragon, mothra, etc.)');
+var newColour = prompt('A colour (orange, indigo, etc.)');
+var newCreature = prompt('A creature (dragon, mothra, etc.)');
 var newAd = prompt('An adjective (beautiful, super, etc.)');
 var newVerb = prompt('A past tense verb (ran, burped, etc.)');
 var newStory = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning');
@@ -16,7 +16,3 @@ if (newStory == 'b') {
 if (newStory == 'c') {
   document.write('Jackson chose his ' + newAd + ', ' + newColour + ', ' + newCreature + ' card and ' + newVerb + ' it to the table knowing he won.');
 }
-
-document.write('After snacking on ' + newAd + ' treats, the ' + newColour + ' bellied ' + newCreature + ' ' + newVerb + ' for hours.');
-document.write('Ameilia ' + newVerb + ' through the ' + newAd + ' ' + newColour + ' nebula escaping the space ' + newCreature);
-document.write('Jackson chose his ' + newAd + ', ' + newColour + ', ' + newCreature + ' card and ' + newVerb + ' it to the table knowing he won.');
