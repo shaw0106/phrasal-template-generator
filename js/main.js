69d0ae997c11a91e0e34ec5ca30b26226c1b89e2
Shannon <shaw0106@algonquinlive.com>
2016-02-26 17:10:25 -0500

    Added boilerplate

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var newName = alert('Let’s make a story together!');
+var newColour = prompt('Pick a colour (orange, indigo, etc.)');
+var newCreature = prompt('Pick a creature (dragon, mothra, etc.)');
+var newAd = prompt('An adjective (beautiful, super, etc.)');
+var newTense = prompt('A past tense verb (ran, burped, etc.)');
+var newStory = prompt('Which story would you like?');
